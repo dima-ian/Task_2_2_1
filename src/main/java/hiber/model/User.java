6624273dae2e9c9066de02ffdf24dd7d02d2823b
Dima Ian <ltr.tsts@mail.ru>
2020-03-22 23:30:44 +0300

    Car + User space lines added

--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -10,10 +10,13 @@ public class User {
    @GeneratedValue(strategy = GenerationType.IDENTITY)
    @Column()
    private Integer id;
+
    @Column()
    private String firstName;
+
    @Column()
    private String lastName;
+
    @Column()
    private String email;
 
